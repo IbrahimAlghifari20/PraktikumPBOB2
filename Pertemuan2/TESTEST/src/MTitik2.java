78afe1a818902c976651f6f10d302cc1d0046779
Ibrahim <mialghifari20@gmail.com>
2026-02-25 16:19:10 +0700

    TesTest

---
+++ Pertemuan2/TESTEST/src/MTitik2.java
@@ -0,0 +1,11 @@
+public class MTitik2 {
+    public static void main(String[] args) {
+        Titik2 T1 = new Titik2();
+        T1.setAbsis(3);
+        T1.setOrdinat(3);
+        T1.printTitik();
+        T1.geser(3, 4);
+        T1.printTitik();
+
+    }   
+}
