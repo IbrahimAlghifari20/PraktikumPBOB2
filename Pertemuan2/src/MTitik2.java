0ed7fd3252656145efaf5d558949b87bd90867dc
Ibrahim <mialghifari20@gmail.com>
2026-02-25 16:02:27 +0700

    Titik Pertemuan2

---
+++ Pertemuan2/src/MTitik2.java
@@ -0,0 +1,16 @@
+public class MTitik2 {
+    public static void main(String[] args) {
+        Titik2 T1 = new Titik2();
+        T1.setAbsis(3);
+        T1.setOrdinat(5);
+        T1.printTitik();
+        T1.geser(3, 4);
+        T1.printTitik();
+        Titik2 T2 = T1;
+        T2.printTitik();
+        T1.setAbsis(10);
+        T1.setOrdinat(10);
+        T2.printTitik();
+
+    }   
+}
